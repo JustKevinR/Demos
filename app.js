5cb2390b38505944c1fc40abb698b79efebad473
KevinPann <kevin.reinov@gmail.com>
2020-04-20 14:00:08 +0300

    4/20

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const textBoxDiv = document.getElementById('text-box')
 const typingArea = document.getElementById('typing-area')
 const feedbackDiv = document.getElementById('feedback')
+const timerBoxDiv = document.getElementById('timer')
 
-
-
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pharetra semper nibh non ornare. Quisque nec sodales libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean semper justo neque, eget pretium erat gravida pulvinar. Nam suscipit volutpat sapien, non pellentesque arcu porttitor et. Cras nec urna nec purus sollicitudin ultricies at non metus. Sed ornare cursus tempus. Mauris eros lacus, sollicitudin et lobortis sed, finibus id odio. Quisque vitae ex quis diam lacinia pharetra. Donec quis leo congue, luctus magna rutrum, iaculis diam. Proin sagittis luctus erat, sed porta mauris viverra nec. Sed efficitur nec magna eget vestibulum. Vivamus vehicula ligula nibh, gravida euismod odio aliquet eget. Fusce at nulla libero. Integer quis sodales sapien."
+const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec nibh ut velit facilisis accumsan. Vestibulum tempor, massa at malesuada gravida, nunc mi pulvinar libero, consequat convallis neque nulla sit amet urna. Proin magna orci, sagittis hendrerit turpis vitae, venenatis pulvinar neque. Mauris gravida, mauris non dictum dignissim, dui est faucibus arcu, quis congue arcu velit vitae dui. In id mauris lectus. Sed sagittis dignissim lorem, sed scelerisque sapien auctor vitae. Quisque at mollis nibh, sit amet scelerisque nulla. Fusce porta pretium odio eget sollicitudin. Vestibulum justo leo, ultrices eget commodo ut, consectetur ac arcu. Maecenas eget ex ut risus mattis porta at vel mi. Mauris vestibulum mi quis diam lacinia, in interdum nunc interdum. Pellentesque faucibus vehicula nisl ut molestie. Nunc porttitor ex a erat consectetur ullamcorper in sed metus. Sed ac magna dignissim, suscipit turpis nec, laoreet eros. Etiam egestas in quam a gravida.'
+let wordsArray = text.split(' ')
 textBoxDiv.innerText = text
 let wordCount = 0
 highlight()
@@ -12,22 +12,38 @@ highlight()
 let errorCount = 0
 feedbackDiv.innerText = errorCount
 
+
+let timer = 0
+
+
+setInterval(() => {
+    timer++
+    minutes = parseInt(timer / 60)
+    seconds = (timer % 60).toString().padStart(2, '0')
+    timerBoxDiv.innerText = minutes + ':' + seconds
+}, 1000);
+
+
 typingArea.focus()
 typingArea.addEventListener('keydown', event => {
     console.log(event.keyCode)
-    
-    if (event.keyCode == 32) {
-        if(text.split(' ')[wordCount] != typingArea.value.split(' ')[wordCount] ) {
+
+    if ( event.keyCode == 32 ) {
+        if ( text.split(' ')[wordCount] != typingArea.value.split(' ')[wordCount] ) {
+            wordsArray[wordCount] = '<span class="incorrect">' + wordsArray[wordCount] + '</span>'
             errorCount++
             feedbackDiv.innerText = errorCount
+        } else {
+            wordsArray[wordCount] = '<span class="correct">' + wordsArray[wordCount] + '</span>'
         }
         wordCount++
         highlight()
     }
+
 })
 
 function highlight () {
-    let wordsArray = text.split(' ')
-    wordsArray[wordCount] = '<span class="highlight">' + wordsArray[wordCount] + '</span>'
-    textBoxDiv.innerHTML = wordsArray.join(' ')
+    let highlightedArray = Array.from(wordsArray)
+    highlightedArray[wordCount] = '<span class="highlight">' + highlightedArray[wordCount] + '</span>'
+    textBoxDiv.innerHTML = highlightedArray.join(' ')
 }
